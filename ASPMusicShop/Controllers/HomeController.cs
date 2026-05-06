4d010642d76e2d651290c87fb13206334beb2639
JustMargata <66246000+JustMargata@users.noreply.github.com>
2026-05-06 16:47:20 +0300

    Added Interesting.cshtml

--- ASPMusicShop/Controllers/HomeController.cs
+++ ASPMusicShop/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ASPMusicShop.Controllers
             return View();
         }
 
+        public IActionResult Interesting()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
