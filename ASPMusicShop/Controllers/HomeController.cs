b59d0c5bf1eeeb02d62ad19ec2e332e3a5feaa32
JustMargata <66246000+JustMargata@users.noreply.github.com>
2026-05-06 17:14:04 +0300

    Added Contacts page

--- ASPMusicShop/Controllers/HomeController.cs
+++ ASPMusicShop/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace ASPMusicShop.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
