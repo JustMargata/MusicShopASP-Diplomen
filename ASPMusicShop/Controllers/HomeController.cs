73dd34baba858aa610cf120b50756fdb599c221f
JustMargata <66246000+JustMargata@users.noreply.github.com>
2026-05-06 16:55:43 +0300

    Added Delivery.cshtml

--- ASPMusicShop/Controllers/HomeController.cs
+++ ASPMusicShop/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ASPMusicShop.Controllers
             return View();
         }
 
+        public IActionResult Delivery()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
